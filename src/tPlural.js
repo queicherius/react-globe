203c296a491c9b22c5064cb280f338d2223032a7
queicherius <queicherius@gmail.com>
2016-06-21 02:30:22 +0200

    Cleaner pluralisation

--- src/tPlural.js
+++ src/tPlural.js
@@ -2,27 +2,41 @@ import i18next from 'i18next'
 import t from './t'
 
 // Formats a pluralized string
-const tPlural = (singular, plural, options = {}) => {
+const tPlural = (messages, options = {}) => {
   if (!Number.isInteger(options.count)) {
-    console.error('[react-globe] tPlural expects a integer "count" option')
+    console.error('[react-globe] tPlural requires a integer "count" option')
+    return null
+  }
+
+  if (!messages.one || !messages.many) {
+    console.error('[react-globe] tPlural requires a "one" and "many" strings')
     return null
   }
 
   // Manually generate english pluralisation based on gettext style
   if (i18next.language === 'en') {
-    return englishPlural(singular, plural, options)
+    return englishPlural(messages, options)
+  }
+
+  // Return an extra string for 0 counts
+  if (options.count === 0) {
+    return t(messages.zero || messages.one, options)
   }
 
   // The translation function figures out plurals for us
-  return t(singular, options)
+  return t(messages.one, options)
 }
 
-const englishPlural = (singular, plural, options) => {
-  if (options.count === 1) {
-    return t(singular, options)
+const englishPlural = (messages, options) => {
+  if (options.count === 0) {
+    return t(messages.zero || messages.many, options)
   }
 
-  return t(plural, options)
+  if (options.count === 1) {
+    return t(messages.one, options)
+  }
+
+  return t(messages.many, options)
 }
 
 export default tPlural
